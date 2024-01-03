ab88f256f0b9eb62c6baec2942b5308fb01889fd
Johan Kok <johan.kok@grabtaxi.com>
2024-01-03 16:00:36 +0800

    Minor fixes

--- python/examples/test_model_training.py
+++ python/examples/test_model_training.py
@@ -10,8 +10,8 @@ from models.VGG16 import Vgg16
 
 torch.manual_seed(0)
 
-model = SimpleMLP(num_classes = 10)
-# model = Vgg16(num_classes = 10)
+# model = SimpleMLP(num_classes = 10)
+model = Vgg16(num_classes = 10)
 
 optimizer = SGD(model.parameters(), lr = 0.001, momentum=0.9)
 
@@ -27,20 +27,20 @@ cl_strategy = Naive(
     plugins = [lwf]
 )
 
-# experience_1_x = torch.zeros(100, 3, 128, 128)
-# experience_1_y = torch.zeros(100, dtype = torch.long)
+experience_1_x = torch.zeros(100, 3, 128, 128)
+experience_1_y = torch.zeros(100, dtype = torch.long)
 
-# test_x = torch.zeros(50, 3, 128, 128)
-# test_y = torch.zeros(50, dtype = torch.long)
+test_x = torch.zeros(50, 3, 128, 128)
+test_y = torch.zeros(50, dtype = torch.long)
 
-# generic_scenario = tensors_benchmark(
-#     train_tensors=[(experience_1_x, experience_1_y)],
-#     test_tensors=[(test_x, test_y)],
-#     task_labels=[0],  # Task label of each train exp
-#     complete_test_set_only=True
-# )
+generic_scenario = tensors_benchmark(
+    train_tensors=[(experience_1_x, experience_1_y)],
+    test_tensors=[(test_x, test_y)],
+    task_labels=[0],  # Task label of each train exp
+    complete_test_set_only=True
+)
 
-generic_scenario = SplitMNIST(n_experiences = 1, seed = 1)
+# generic_scenario = SplitMNIST(n_experiences = 1, seed = 1)
 
 # TRAINING LOOP
 print('Starting experiment...')
